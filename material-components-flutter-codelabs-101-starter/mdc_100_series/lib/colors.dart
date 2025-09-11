449119049125de9aa6866bbfb931be2dbc482f9d
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-09-11 17:43:38 -0300

    adicionar constantes com as cores da marca

---
+++ material-components-flutter-codelabs-101-starter/mdc_100_series/lib/colors.dart
@@ -0,0 +1,16 @@
+/* Definição de cores do projeto. */
+import "package:flutter/material.dart";
+
+/* Tonalidade das cores da marca */
+// Rosa
+const kShrinePink50 = Color(0xFFFEEAE6);
+const kShrinePink100 = Color(0xFFFEDBD0);
+const kShrinePink300 = Color(0xFFFBB8AC);
+const kShrinePink400 = Color(0xFFEAA4A4);
+// Marrom
+const kShrineBrown900 = Color(0xFF442B2D);
+// Vermelho
+const kShrineErrorRed = Color(0xFFC5032B);
+// Branco
+const kShrineSurfaceWhite = Color(0xFFFFFBFA);
+const kShrineBackgroundWhite = Colors.white;
