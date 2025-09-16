633f0d860fe9b3a8dd0c39b6791330231d309345
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-09-16 10:22:16 -0300

    extrair widget `appBar` em um método próprio

--- fitness/lib/pages/home.dart
+++ fitness/lib/pages/home.dart
@@ -8,54 +8,57 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Layout Scaffold
-    return Scaffold(
-      appBar: AppBar(
-        // Aparência da AppBar
-        backgroundColor: Colors.white,
-        elevation: 0,
-        // Título da página
-        title: Text(
-          "Breakfast",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
+    return Scaffold(appBar: appBar());
+  }
+
+  /// Barra superior do app.
+  AppBar appBar() {
+    return AppBar(
+      // Aparência da AppBar
+      backgroundColor: Colors.white,
+      elevation: 0,
+      // Título da página
+      title: Text(
+        "Breakfast",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
         ),
-        centerTitle: true,
-        // Botão de voltar (início da appBar)
-        leading: GestureDetector(
+      ),
+      centerTitle: true,
+      // Botão de voltar (início da appBar)
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            // quadrado com background ao redor (parecer um botão)
+            color: Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Icon(LucideIcons.chevronLeft),
+        ),
+      ),
+      // Botões no fim da appBar
+      actions: [
+        GestureDetector(
           onTap: () {},
           child: Container(
             margin: EdgeInsets.all(10),
             alignment: Alignment.center,
+            width: 37,
+            height: 37,
             decoration: BoxDecoration(
               // quadrado com background ao redor (parecer um botão)
               color: Color(0xffF7F8F8),
               borderRadius: BorderRadius.circular(10),
             ),
-            child: Icon(LucideIcons.chevronLeft),
+            child: Icon(LucideIcons.menu),
           ),
         ),
-        // Botões no fim da appBar
-        actions: [
-          GestureDetector(
-            onTap: () {},
-            child: Container(
-              margin: EdgeInsets.all(10),
-              alignment: Alignment.center,
-              width: 37,
-              height: 37,
-              decoration: BoxDecoration(
-                // quadrado com background ao redor (parecer um botão)
-                color: Color(0xffF7F8F8),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Icon(LucideIcons.menu),
-            ),
-          ),
-        ],
-      ),
+      ],
     );
   }
 }
