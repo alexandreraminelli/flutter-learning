3661cb7dcf6319e1c72ee2a96bcfb264402836f1
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-09-16 09:03:32 -0300

    criar arquivo e widget da página inicial

---
+++ fitness/lib/pages/home.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+
+/// Página inicial do app
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
