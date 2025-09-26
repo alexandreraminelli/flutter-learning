196e02c646707f824f429c390e8c4526e1ee2b46
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-09-26 10:20:52 -0300

    Stateful Widgets

--- ultimate_flutter_tutorial/lib/main.dart
+++ ultimate_flutter_tutorial/lib/main.dart
@@ -19,31 +19,56 @@ class MyApp extends StatelessWidget {
         ),
       ),
       // Página inicial do app
-      home: Scaffold(
-        // Conteúdo do layout mobile
-        appBar: AppBar(
-          title: Text("Flutter App"), // Título da página
-          centerTitle: true,
-        ),
-        bottomNavigationBar: NavigationBar(
-          // Lista de destinos do app
-          destinations: [
-            NavigationDestination(
-              icon: Icon(Icons.home_rounded),
-              label: "Início",
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.person_rounded),
-              label: "Perfil",
-            ),
-          ],
-          selectedIndex: 0, // índice atual
-          // Função chamada ao selecionar um destino
-          // value: índice do destino selecionado
-          onDestinationSelected: (int value) {
-            // TODO: navegação entre as páginas
-          },
-        ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  /// Índice da página atual
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Conteúdo do layout mobile
+      appBar: AppBar(
+        title: Text("Flutter App"), // Título da página
+        centerTitle: true,
+      ),
+
+      body: Center(child: currentIndex == 0 ? Text("Início") : Text("Perfil")),
+
+      bottomNavigationBar: NavigationBar(
+        // Lista de destinos do app
+        destinations: [
+          NavigationDestination(
+            icon: Icon(Icons.home_rounded),
+            label: "Início",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person_rounded),
+            label: "Perfil",
+          ),
+        ],
+        selectedIndex: currentIndex, // índice atual
+        // Função chamada ao selecionar um destino
+        // value: índice do destino selecionado
+        onDestinationSelected: (int value) {
+          // Tornar destino selecionado o atual
+          setState(() {
+            currentIndex = value;
+          });
+
+          // TODO: navegação entre as páginas
+        },
       ),
     );
   }
