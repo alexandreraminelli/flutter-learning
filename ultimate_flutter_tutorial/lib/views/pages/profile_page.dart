3baad6fc02ecac24d891d473cf306841cbd3a23c
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-09-29 17:00:01 -0300

    SingleChildScrollView

--- ultimate_flutter_tutorial/lib/views/pages/profile_page.dart
+++ ultimate_flutter_tutorial/lib/views/pages/profile_page.dart
@@ -22,65 +22,71 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          // Input
-          TextField(
-            controller: controller,
-            decoration: InputDecoration(border: OutlineInputBorder()),
-            onEditingComplete: () => setState(() {}),
-          ),
-          // Output
-          Text(controller.text),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            // Input
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(border: OutlineInputBorder()),
+              onEditingComplete: () => setState(() {}),
+            ),
+            // Output
+            Text(controller.text),
 
-          // Checkbox Simples
-          Checkbox.adaptive(
-            tristate: true, // 3º estado (indeterminado/null)
-            value: isChecked,
-            onChanged: (bool? value) {
-              setState(() {
+            // Checkbox Simples
+            Checkbox.adaptive(
+              tristate: true, // 3º estado (indeterminado/null)
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            // Lista de Checkboxes
+            CheckboxListTile.adaptive(
+              tristate: true, // 3º estado (indeterminado/null)
+              title: Text("Clique em Mim"),
+              value: isChecked,
+              onChanged: (bool? value) => setState(() {
                 isChecked = value;
-              });
-            },
-          ),
-          // Lista de Checkboxes
-          CheckboxListTile.adaptive(
-            tristate: true, // 3º estado (indeterminado/null)
-            title: Text("Clique em Mim"),
-            value: isChecked,
-            onChanged: (bool? value) => setState(() {
-              isChecked = value;
-            }),
-          ),
+              }),
+            ),
 
-          Switch.adaptive(
-            value: isSwitched,
-            onChanged: (value) => setState(() {
-              isSwitched = value;
-            }),
-          ),
+            Switch.adaptive(
+              value: isSwitched,
+              onChanged: (value) => setState(() {
+                isSwitched = value;
+              }),
+            ),
 
-          SwitchListTile.adaptive(
-            title: Text("Switch Title"),
-            subtitle: Text("Switch Subtitle"),
-            value: isSwitched,
-            onChanged: (value) => setState(() {
-              isSwitched = value;
-            }),
-          ),
+            SwitchListTile.adaptive(
+              title: Text("Switch Title"),
+              subtitle: Text("Switch Subtitle"),
+              value: isSwitched,
+              onChanged: (value) => setState(() {
+                isSwitched = value;
+              }),
+            ),
 
-          Slider.adaptive(
-            value: sliderValue,
-            max: 100.0,
-            divisions: 10, // limitar valores do slider a serem múltiplos de D
-            onChanged: (double value) => setState(() {
-              sliderValue = value;
-            }),
-          ),
-          Text(sliderValue.toString()), // valor do Slider
-        ],
+            Slider.adaptive(
+              value: sliderValue,
+              max: 100.0,
+              divisions: 10, // limitar valores do slider a serem múltiplos de D
+              onChanged: (double value) => setState(() {
+                sliderValue = value;
+              }),
+            ),
+            Text(sliderValue.toString()), // valor do Slider
+
+            Image.asset("assets/images/bg.jpg"),
+            SizedBox(height: 14),
+            Image.asset("assets/images/bg.jpg"),
+          ],
+        ),
       ),
     );
   }
