cd54acdfe3f50ebf8bbf1a89b9ee10515a77eaf0
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-01 19:12:43 -0300

    UI modification (again & again)

--- ultimate_flutter_tutorial/lib/data/constants.dart
+++ ultimate_flutter_tutorial/lib/data/constants.dart
@@ -17,3 +17,10 @@ class KTextStyle {
   /// Título de descrição.
   static const TextStyle descriptionText = TextStyle(fontSize: 16.0);
 }
+
+class KValue {
+  static const String basicLayout = "Basic Layout";
+  static const String cleanUi = "Clean UI";
+  static const String fixBugs = "Fix Bugs";
+  static const String keyConcepts = "Key Concepts";
+}
--- ultimate_flutter_tutorial/lib/views/pages/home_page.dart
+++ ultimate_flutter_tutorial/lib/views/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ultimate_flutter_tutorial/data/constants.dart';
+import 'package:ultimate_flutter_tutorial/views/pages/course_page.dart';
 import 'package:ultimate_flutter_tutorial/views/widgets/container_widget.dart';
 import 'package:ultimate_flutter_tutorial/views/widgets/hero_widget.dart';
 
@@ -7,24 +9,30 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    /// Lista de conteúdo dos cards
+    List<String> list = [
+      KValue.keyConcepts,
+      KValue.cleanUi,
+      KValue.fixBugs,
+      KValue.basicLayout,
+    ];
+
     return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 20.0),
         child: Column(
           children: [
-            HeroWidget(title: "Flutter Mapp"), // imagem para animação
+            HeroWidget(title: "Flutter Mapp", nextPage: CoursePage()),
             // Cards
-            ContainerWidget(
-              title: "Basic Layout",
-              description: "This is a description",
-            ),
-            ContainerWidget(
-              title: "Basic Layout",
-              description: "This is a description",
-            ),
-            ContainerWidget(
-              title: "Basic Layout",
-              description: "This is a description",
+            ...List.generate(
+              list.length, // length (quant. de elementos)
+              (index) {
+                // generator (função que gera os Widgets)
+                return ContainerWidget(
+                  title: list[index],
+                  description: "This is a description",
+                );
+              },
             ),
           ],
         ),
