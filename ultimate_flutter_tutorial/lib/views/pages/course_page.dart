41e470e2b91a10d92c06039de1db5e2b032c38e9
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-03 09:06:41 -0300

    networking-api: Get

--- ultimate_flutter_tutorial/lib/views/pages/course_page.dart
+++ ultimate_flutter_tutorial/lib/views/pages/course_page.dart
@@ -1,8 +1,42 @@
+import "dart:convert";
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:ultimate_flutter_tutorial/views/widgets/hero_widget.dart';
 
-class CoursePage extends StatelessWidget {
+class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
+
+  @override
+  State<CoursePage> createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  @override
+  void initState() {
+    getData(); // Obter dados da API
+    super.initState();
+  }
+
+  /// Obter dados da API
+  void getData() async {
+    var url = Uri.https(
+      "https://bored-api.appbrewery.com", // URL da API
+      '/random', // endpoint da API
+      {'q': '{http}'}, // query parameters (após ?)
+    );
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      /* Requisição bem-sucedida */
+      var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
+      var itemCount = jsonResponse['activity'];
+      print('$itemCount');
+    } else {
+      /* Requisição mal-sucedida */
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
