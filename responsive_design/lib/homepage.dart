81476c34eaef46715d55ff82325a668ac4a77970
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-05 14:01:06 -0300

    Explicação sobre MediaQuery

--- responsive_design/lib/homepage.dart
+++ responsive_design/lib/homepage.dart
@@ -10,8 +10,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final currentWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      
+      backgroundColor: currentWidth < 600
+          ? Colors.deepPurple[300]
+          : Colors.green[300],
+      body: Center(child: Text(currentWidth.toString())),
     );
   }
 }
